2b147bc4e7f279cf0f457666ca4822b9cfd361c2
TatianaBurmistrova <burmistrova.email@gmail.com>
2022-09-15 21:59:07 +0300

    2.9 group modification

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -15,22 +15,26 @@ public class GroupHelper extends HelperBase {
   public void submitGroupCreation() {
     click(By.name("submit"));
   }
-
   public void fillGroupForm(GroupData groupData) {
     type(By.name("group_name"), groupData.getName());
     type(By.name("group_header"), groupData.getHeader());
     type(By.name("group_footer"), groupData.getFooter());
   }
-
   public void initGroupCreation() {
     click(By.name("new"));
   }
-
   public void deleteSelectedGroups() {
     click(By.name("delete"));
   }
-
   public void selectGroup() {
     click(By.name("selected[]"));
+  }
+
+  public void initGroupModification() {
+    click(By.name("edit"));
+  }
+
+  public void submitGroupModification() {
+    click(By.name("update"));
   }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -10,11 +10,9 @@ public class NavigationHelper extends HelperBase {
   public void goToGroupPage() {
     click(By.linkText("groups"));
   }
-
   public void returnToHomePage() {
     click(By.linkText("home page"));
   }
-
   public void goToHomePage() {
     click(By.linkText("home"));
   }
